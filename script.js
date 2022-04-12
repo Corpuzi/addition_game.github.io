fb301491eee637de3b002cd2bdd46290de9d434e
Jonmar Corpuz <99892186+Corpuzi@users.noreply.github.com>
2022-04-12 00:00:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,29 +16,39 @@ const INCORRECT = [
 ]
 
 function randomEquation() {
+    //Generate random numbers for our addition equation
     let randomNumberOne = Math.floor(Math.random() * (NUMBERS.length));
     let randomNumberTwo = Math.floor(Math.random() * (NUMBERS.length));
     let randomNumberThree = Math.floor(Math.random() * (NUMBERS.length));
     let randomNumberFour = Math.floor(Math.random() * (NUMBERS.length));
 
+    //Generate random numbers for success and fail messages
     let randomTrue = Math.floor(Math.random() * (CORRECT.length));
     let randomFalse = Math.floor(Math.random() * (INCORRECT.length));
 
+    //Generate & display a random addition equation as a string
+    let equation = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]} + ${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`; 
     
-
-    let equation = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]} + ${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`;
-    //document.getElementById("displayEquation").innerText = equation; 
+    //Store the first random generated number of the equation inside a variable
     let numberOne = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]}`;
+    
+    //Store the second random generated number of the equation inside a variable
     let numberTwo = `${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`;
 
+    //Convert the random generated numbers from strings to integers
     let intOne = parseInt(numberOne);
     let intTwo = parseInt(numberTwo);
 
+    //Create a variable that adds the number value of both random generated numbers
     let equationAnswer = intOne + intTwo;
 
+    //Prompt the user to answer the equation
     let showEquation = prompt(`What's ${equation} ?`);
+    
+    //Create a variable to store the success or fail message
     let result;
 
+    //Verify if the input equals the correct answer and return the final result
     if (showEquation == equationAnswer) {
         result = CORRECT[randomTrue];
     } else if (showEquation == "") {
@@ -46,7 +56,7 @@ function randomEquation() {
     } else {
         result = INCORRECT[randomFalse];
     }
-
-
+    
+    //Display the result in index.html
     document.getElementById("result").innerText = result;
 }
