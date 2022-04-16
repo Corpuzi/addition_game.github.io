2f4d782a6e88bce56dcd6eb9ddf7a0188d78cf2e
Jonmar Corpuz <99892186+Corpuzi@users.noreply.github.com>
2022-04-15 22:41:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,13 +17,15 @@ const INCORRECT = [
     "Wrong!"
 ]
 
-function randomEquation() {
+
+function randomAddition() {
+
     //Generate random numbers for our addition equation
     let randomNumberOne = Math.floor(Math.random() * (NUMBERS.length));
     let randomNumberTwo = Math.floor(Math.random() * (NUMBERS.length));
     let randomNumberThree = Math.floor(Math.random() * (NUMBERS.length));
     let randomNumberFour = Math.floor(Math.random() * (NUMBERS.length));
-
+    
     //Generate random numbers for success and fail messages
     let randomTrue = Math.floor(Math.random() * (CORRECT.length));
     let randomFalse = Math.floor(Math.random() * (INCORRECT.length));
@@ -54,7 +56,7 @@ function randomEquation() {
     if (addition == equationAnswer) {
         result = CORRECT[randomTrue];
     } else if (addition == "") {
-        result = "Please type in something next time";
+        result = "Error: Please type in something next time";
     } else {
         result = INCORRECT[randomFalse];
     }
@@ -63,3 +65,237 @@ function randomEquation() {
     document.getElementById("result").innerText = result;
 }
 
+function randomSubtraction() {
+    
+    //Generate random numbers for our addition equation
+    let randomNumberOne = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberTwo = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberThree = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberFour = Math.floor(Math.random() * (NUMBERS.length));
+    
+    //Generate random numbers for success and fail messages
+    let randomTrue = Math.floor(Math.random() * (CORRECT.length));
+    let randomFalse = Math.floor(Math.random() * (INCORRECT.length));
+
+    //Generate & display a random addition equation as a string
+    let equation = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]} - ${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`; 
+    
+    //Store the first random generated number of the equation inside a variable
+    let numberOne = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]}`;
+    
+    //Store the second random generated number of the equation inside a variable
+    let numberTwo = `${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`;
+
+    //Convert the random generated numbers from strings to integers
+    let intOne = parseInt(numberOne);
+    let intTwo = parseInt(numberTwo);
+
+    //Create a variable that subtracts the number value of both random generated numbers
+    let equationAnswer = intOne - intTwo;
+
+    //Prompt the user to answer the equation
+    let addition = prompt(`What's ${equation} ?`);
+    
+    //Create a variable to store the success or fail message
+    let result;
+
+    //Verify if the input equals the correct answer and return the final result
+    if (addition == equationAnswer) {
+        result = CORRECT[randomTrue];
+    } else if (addition == "") {
+        result = "Error: Please type in something next time";
+    } else {
+        result = INCORRECT[randomFalse];
+    }
+    
+    //Display the result in index.html
+    document.getElementById("result").innerText = result;
+}
+
+function randomAddition() {
+
+    //Generate random numbers for our addition equation
+    let randomNumberOne = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberTwo = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberThree = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberFour = Math.floor(Math.random() * (NUMBERS.length));
+    
+    //Generate random numbers for success and fail messages
+    let randomTrue = Math.floor(Math.random() * (CORRECT.length));
+    let randomFalse = Math.floor(Math.random() * (INCORRECT.length));
+
+    //Generate & display a random addition equation as a string
+    let equation = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]} + ${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`; 
+    
+    //Store the first random generated number of the equation inside a variable
+    let numberOne = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]}`;
+    
+    //Store the second random generated number of the equation inside a variable
+    let numberTwo = `${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`;
+
+    //Convert the random generated numbers from strings to integers
+    let intOne = parseInt(numberOne);
+    let intTwo = parseInt(numberTwo);
+
+    //Create a variable that adds the number value of both random generated numbers
+    let equationAnswer = intOne + intTwo;
+
+    //Prompt the user to answer the equation
+    let addition = prompt(`What's ${equation} ?`);
+    
+    //Create a variable to store the success or fail message
+    let result;
+
+    //Verify if the input equals the correct answer and return the final result
+    if (addition == equationAnswer) {
+        result = CORRECT[randomTrue];
+    } else if (addition == "") {
+        result = "Error: Please type in something next time";
+    } else {
+        result = INCORRECT[randomFalse];
+    }
+    
+    //Display the result in index.html
+    document.getElementById("result").innerText = result;
+}
+
+function randomMultiplication() {
+    
+    //Generate random numbers for our addition equation
+    let randomNumberOne = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberTwo = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberThree = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberFour = Math.floor(Math.random() * (NUMBERS.length));
+    
+    //Generate random numbers for success and fail messages
+    let randomTrue = Math.floor(Math.random() * (CORRECT.length));
+    let randomFalse = Math.floor(Math.random() * (INCORRECT.length));
+
+    //Generate & display a random addition equation as a string
+    let equation = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]} * ${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`; 
+    
+    //Store the first random generated number of the equation inside a variable
+    let numberOne = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]}`;
+    
+    //Store the second random generated number of the equation inside a variable
+    let numberTwo = `${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`;
+
+    //Convert the random generated numbers from strings to integers
+    let intOne = parseInt(numberOne);
+    let intTwo = parseInt(numberTwo);
+
+    //Create a variable that subtracts the number value of both random generated numbers
+    let equationAnswer = intOne * intTwo;
+
+    //Prompt the user to answer the equation
+    let addition = prompt(`What's ${equation} ?`);
+    
+    //Create a variable to store the success or fail message
+    let result;
+
+    //Verify if the input equals the correct answer and return the final result
+    if (addition == equationAnswer) {
+        result = CORRECT[randomTrue];
+    } else if (addition == "") {
+        result = "Error: Please type in something next time";
+    } else {
+        result = INCORRECT[randomFalse];
+    }
+    
+    //Display the result in index.html
+    document.getElementById("result").innerText = result;
+}
+
+function randomAddition() {
+
+    //Generate random numbers for our addition equation
+    let randomNumberOne = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberTwo = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberThree = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberFour = Math.floor(Math.random() * (NUMBERS.length));
+    
+    //Generate random numbers for success and fail messages
+    let randomTrue = Math.floor(Math.random() * (CORRECT.length));
+    let randomFalse = Math.floor(Math.random() * (INCORRECT.length));
+
+    //Generate & display a random addition equation as a string
+    let equation = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]} + ${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`; 
+    
+    //Store the first random generated number of the equation inside a variable
+    let numberOne = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]}`;
+    
+    //Store the second random generated number of the equation inside a variable
+    let numberTwo = `${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`;
+
+    //Convert the random generated numbers from strings to integers
+    let intOne = parseInt(numberOne);
+    let intTwo = parseInt(numberTwo);
+
+    //Create a variable that adds the number value of both random generated numbers
+    let equationAnswer = intOne + intTwo;
+
+    //Prompt the user to answer the equation
+    let addition = prompt(`What's ${equation} ?`);
+    
+    //Create a variable to store the success or fail message
+    let result;
+
+    //Verify if the input equals the correct answer and return the final result
+    if (addition == equationAnswer) {
+        result = CORRECT[randomTrue];
+    } else if (addition == "") {
+        result = "Error: Please type in something next time";
+    } else {
+        result = INCORRECT[randomFalse];
+    }
+    
+    //Display the result in index.html
+    document.getElementById("result").innerText = result;
+}
+
+function randomDivision() {
+    
+    //Generate random numbers for our addition equation
+    let randomNumberOne = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberTwo = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberThree = Math.floor(Math.random() * (NUMBERS.length));
+    let randomNumberFour = Math.floor(Math.random() * (NUMBERS.length));
+    
+    //Generate random numbers for success and fail messages
+    let randomTrue = Math.floor(Math.random() * (CORRECT.length));
+    let randomFalse = Math.floor(Math.random() * (INCORRECT.length));
+
+    //Generate & display a random addition equation as a string
+    let equation = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]} / ${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`; 
+    
+    //Store the first random generated number of the equation inside a variable
+    let numberOne = `${NUMBERS[randomNumberOne]}${NUMBERS[randomNumberTwo]}`;
+    
+    //Store the second random generated number of the equation inside a variable
+    let numberTwo = `${NUMBERS[randomNumberThree]}${NUMBERS[randomNumberFour]}`;
+
+    //Convert the random generated numbers from strings to integers
+    let intOne = parseInt(numberOne);
+    let intTwo = parseInt(numberTwo);
+
+    //Create a variable that subtracts the number value of both random generated numbers
+    let equationAnswer = intOne / intTwo;
+
+    //Prompt the user to answer the equation
+    let addition = prompt(`What's ${equation} ?`);
+    
+    //Create a variable to store the success or fail message
+    let result;
+
+    //Verify if the input equals the correct answer and return the final result
+    if (addition == equationAnswer) {
+        result = CORRECT[randomTrue];
+    } else if (addition == "") {
+        result = "Error: Please type in something next time";
+    } else {
+        result = INCORRECT[randomFalse];
+    }
+    
+    //Display the result in index.html
+    document.getElementById("result").innerText = result;
+}
